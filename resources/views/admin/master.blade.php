9ef03284d78b046ec3061009ed9726e5bb4eb1b2
shaima elfiqi <shimo11shimo11@gmail.com>
2024-01-17 09:09:41 +0200

    12

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+
+<html
+  lang="en"
+  class="light-style layout-menu-fixed"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="{{asset("assets")}}/"
+  data-template="vertical-menu-template-free"
+>
+@include('admin.partials.head')
+
+
+  <body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+      <div class="layout-container">
+        <!-- Menu -->
+        @include('admin.partials.sidebar')
+
+        <!-- / Menu -->
+
+        <!-- Layout container -->
+        <div class="layout-page">
+          <!-- Navbar -->
+     @include('admin.partials.navbare')
+
+
+
+          <div class="content-wrapper">
+            <!-- Content -->
+       @yield('content')
+
+@include('admin.partials.footer')
+
+
+            <div class="content-backdrop fade"></div>
+          </div>
+          <!-- Content wrapper -->
+        </div>
+        <!-- / Layout page -->
+      </div>
+
+      <!-- Overlay -->
+      <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
+
+ @include('admin.partials.scripts')
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+
+  </body>
+</html>
